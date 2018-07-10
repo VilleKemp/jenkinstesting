c5452c5408a26ff1b26a553ae899c48f48895bc2
VilleKemp <kempo.ville@gmail.com>
2018-07-10 10:36:08 +0300

    added dependency download using pip

--- environment/get/get_git.sh
+++ environment/get/get_git.sh
@@ -2,3 +2,6 @@
 git clone --depth=1 https://github.com/VilleKemp/SeleniumTesting.git
 #etracker
 git clone -b WIP https://github.com/VilleKemp/ExerciseTracker.git
+
+pip install flask flask_restful
+
