aa6bf0d973379ef12b8e63fd45cc344583b87b45
VilleKemp <kempo.ville@gmail.com>
2018-07-11 11:42:41 +0300

    made curl directed to mutillidae silent

--- environment/setup/setup_browsermob.sh
+++ environment/setup/setup_browsermob.sh
@@ -7,7 +7,7 @@ echo "curl"
 curl -X PUT -d "captureContent=true" -d "captureHeaders=true" -d "captureCookies=true" -d "captureBinaryContent=true" http://localhost:8181/proxy/8081/har 
 sleep 1
 echo "curl"
-# Set mutillidae database
-curl http://localhost:81/mutillidae/set-up-database.php
+# Set mutillidae database. Silence output due to its huge size
+curl -s http://localhost:81/mutillidae/set-up-database.php > /dev/null
 sleep 1
 
