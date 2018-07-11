9b59930db33b1a1cfc49597306383daa6d8b4c8c
VilleKemp <kempo.ville@gmail.com>
2018-07-11 10:38:23 +0300

    m

--- environment/setup/setup_browsermob.sh
+++ environment/setup/setup_browsermob.sh
@@ -3,3 +3,7 @@ curl -X POST -d "port=8081" http://localhost:8181/proxy
 #There were some issues of proxy not working properly. Assumed it was due to these commands executing too fast.
 sleep 1
 curl -X PUT -d "captureContent=true" -d "captureHeaders=true" -d "captureCookies=true" -d "captureBinaryContent=true" http://localhost:8181/proxy/8081/har 
+
+# Set mutillidae database
+curl http://local/mutillidae/set-up-database.php
+
