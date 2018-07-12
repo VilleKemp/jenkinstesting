1ebfd2dfb67fce69c6534e7fc2741edf530d9033
kkk <kkk@kkk.fi>
2018-07-12 10:02:13 +0300

    new command with abs path

--- environment/defensics/defensics_launch.sh
+++ environment/defensics/defensics_launch.sh
@@ -1,2 +1,3 @@
 #Change paths to correspond your own patchs
-java -jar /opt/Synopsys/Defensics/monitor/boot.jar --suite /home/jukka/synopsys/defensics/suites/web-app-3.6.0/testtool/web-app-360.jar --sequence defensics/mutillidaefuzz.seq --index 1-300
+#java -jar /opt/Synopsys/Defensics/monitor/boot.jar --suite /home/jukka/synopsys/defensics/suites/web-app-3.6.0/testtool/web-app-360.jar --sequence mutillidaefuzz.seq --index 1-300
+java -jar /opt/Synopsys/Defensics/monitor/boot.jar --suite /home/jukka/synopsys/defensics/suites/web-app-3.6.0/testtool/web-app-360.jar --sequence /home/jukka/Jenkinsstuff/jenkinstesting/environment/defensics/sequence/mutillidaefuzz.seq --index 1-300
