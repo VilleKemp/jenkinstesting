63e7d504f7f1534a02c1148eafe4477024c5f680
VilleKemp <kempo.ville@gmail.com>
2018-07-16 15:22:31 +0300

    changed locate to search for folder/file combo and not just boot.jar

--- environment/defensics/defensics_launch.sh
+++ environment/defensics/defensics_launch.sh
@@ -1,7 +1,7 @@
 #$1 is sequence file location
 # There might be a better way. 
-bootjar=$(locate -i boot.jar)
-suite=$(locate -i web-app-360.jar)
+bootjar=$(locate -i Defensics/monitor/boot.jar)
+suite=$(locate -i /testtool/web-app-360.jar)
 
 java -jar $bootjar --set-license-flex-addr @100.66.228.229
 java -jar $bootjar --suite $suite --sequence $1 --index 1-300 || true
