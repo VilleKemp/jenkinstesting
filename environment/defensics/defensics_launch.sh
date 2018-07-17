d13e1eeea12b7e302704179c1cab6b337df16daa
VilleKemp <kempo.ville@gmail.com>
2018-07-17 13:39:55 +0300

    added verbose flag

--- environment/defensics/defensics_launch.sh
+++ environment/defensics/defensics_launch.sh
@@ -4,4 +4,4 @@ bootjar=$(locate -i Defensics/monitor/boot.jar)
 suite=$(locate -i /testtool/web-app-360.jar)
 
 java -jar $bootjar --set-license-flex-addr @100.66.228.229
-java -jar $bootjar --suite $suite --sequence $1 --index 1-300 --verbose || true
+java -jar $bootjar --verbose --suite $suite --sequence $1 --index 1-300 || true
