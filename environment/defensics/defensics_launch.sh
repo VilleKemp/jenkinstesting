3cdb0d63cb224ff12124d9c7e6301f77a7ceafe9
VilleKemp <kempo.ville@gmail.com>
2018-07-18 12:06:41 +0300

    print and hifistely

--- environment/defensics/defensics_launch.sh
+++ environment/defensics/defensics_launch.sh
@@ -2,13 +2,15 @@
 #$2 har file location
 #$3 workspace
 str=$3
+targethost=localhost
 # There might be a better way. 
 bootjar=$(locate -i Defensics/monitor/boot.jar)
 suite=$(locate -i /testtool/web-app-360.jar)
 echo "LICENCE THINGS"
 java -jar $bootjar --set-license-flex-addr @100.66.228.229
 echo "SEQUENCE CREATION"
-java -jar $bootjar --create-sequence $2 --save-sequence-path $1 --target-hosts localhost
+echo "CREATING SEQUENCE FROM HAR IN $2 TO $1 WITH THE TARGET HOST $targethost"
+java -jar $bootjar --create-sequence $2 --save-sequence-path $1 --target-hosts $targethost
 echo "TESTING"
 java -jar $bootjar --verbose --suite $suite --sequence $1 --index 1-300 || true
 echo "RESULTS"
